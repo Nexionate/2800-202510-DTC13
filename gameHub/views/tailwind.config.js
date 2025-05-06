7f2e22f02f14ec3779fc04d630cf4b8ddc8b21a9
Ethan O'Connor <ethan@northnode.ca>
2025-05-06 11:23:22 -0700

    Create frontend design

---
+++ gameHub/views/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily:
+            {
+                'oswald': ['Oswald', 'sans-serif'],
+                'Fira': ['Fira Sans', 'sans-serif'],
+            }
+        },
+    },
+}
